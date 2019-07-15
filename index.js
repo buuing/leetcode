c7d82a30e3372f8d7781893cd752126e5b00cf42
DESKTOP-B7NUKFJ\DELL <17778888138@189.cn>
2019-07-15 18:21:45 +0800

    草稿

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 /**
  * @Author { String } ldq
  */
-const minSubArrayLen = (s, arr) => {
-  if (arr.length === 0) return 0
-  let min = arr.length, sum = 0, i = 0, j = 0
-  while (i === arr.length - 1) {
-    
+const addTwoNumbers = (a, b) => {
+  let carry = 0, arr = [], res = {}
+  while (a.val !== 0 || b.val !== 0) {
+    let s = a.val + b.val + carry
+    if (s > 9) carry = 1
+    else carry = 0
+    arr.push(s % 10)
+    a = a.next ? a.next : { val: 0, next: null }
+    b = b.next ? b.next : { val: 0, next: null }
   }
-  return min
+  for (let i = arr.length - 1; i >= 0; i--) {
+    res.val = 
+  }
 }
 
 console.log(JSON.stringify(
-  minSubArrayLen(7, [2,3,1,2,4,3])
+  addTwoNumbers(
+    {"val":2,"next":{"val":4,"next":{"val":3,"next":null}}},
+    {"val":5,"next":{"val":6,"next":{"val":4,"next":{"val":9,"next":null}}}}
+  )
 ))
