09a877f2b0da95a6c0af240736ee82b96c37554d
buuing <17778888138@189.cn>
2020-04-21 22:01:28 +0800

    二叉树中序遍历

--- medium/94.binary-tree-inorder-traversal.js
+++ medium/94.binary-tree-inorder-traversal.js
@@ -9,3 +9,20 @@ const inorderTraversal = root => {
   res.push(...inorderTraversal(root.right))
   return res
 }
+
+/**
+ * 中序迭代 (这个是官网题解, 思路完美, 时间复杂度为2n)
+ */
+const inorderTraversal = root => {
+  let stack = [], res = []
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()
+    res.push(root.val)
+    root = root.right
+  }
+  return res
+}
--- medium/98.validate-binary-search-tree.js
+++ medium/98.validate-binary-search-tree.js
@@ -1,4 +1,7 @@
 
+/**
+ * 递归解题
+ */
 const isValidBST = root => {
   if (!root) return true
   const _deep = (node, min, max) => {
@@ -8,4 +11,22 @@ const isValidBST = root => {
     return _deep(node.left, min, node.val) && _deep(node.right, node.val, max)
   }
   return _deep(root.left, -Infinity, root.val) && _deep(root.right, root.val, Infinity)
+}
+
+/**
+ * 中序遍历二叉搜索树会得到一个升序的数组
+ */
+const isValidBST = root => {
+  let stack = [], num = -Infinity
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()
+    if (root.val <= num) return false
+    num = root.val
+    root = root.right
+  }
+  return true
 }
