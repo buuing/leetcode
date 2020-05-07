f6ed55c65149679c30f5634f85edfdf39b6624d6
buuing <17778888138@189.cn>
2020-05-07 14:06:40 +0800

    动态规划

--- easy/70.climbing-stairs.js
+++ easy/70.climbing-stairs.js
@@ -6,4 +6,23 @@ const climbStairs = n => {
 }
 
 // 动态规划
+const climbStairs = n => {
+  const map = [0, 1, 2]
+  for (let i = 3; i <= n; i++) {
+    map[i] = map[i - 1] + map[i - 2]
+  }
+  return map[n]
+}
 
+// 迭代
+const climbStairs = n => {
+  if (n === 1) return 1
+  if (n === 2) return 2
+  let left = 1, right = 2, res
+  for (let i = 3; i <= n; i++) {
+    res = left + right
+    left = right
+    right = res
+  }
+  return res
+}
