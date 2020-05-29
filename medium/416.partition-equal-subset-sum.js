daf4cea95cdb3cb2b3a27b5abb196a1272d605c5
buuing <17778888138@189.cn>
2020-05-29 16:46:11 +0800

    等分数组动态规划

--- medium/416.partition-equal-subset-sum.js
+++ medium/416.partition-equal-subset-sum.js
@@ -12,3 +12,41 @@ const canPartition = nums => {
   }
   return computeSum(len - 1, sum / 2)
 }
+
+// 动态规划
+const canPartition = nums => {
+  let len = nums.length, sum = 0
+  for (let i = 0; i < len; i++) {
+    sum += nums[i]
+  }
+  if (sum % 2 !== 0) return false
+  sum = sum / 2
+  let dp = [[1]]
+  for (let i = 1; i <= len; i++) { // 物品序号
+    dp[i] = [1]
+    let curr = nums[i - 1]
+    for (let j = 1; j <= sum; j++) { // 背包容量
+      dp[i][j] = dp[i - 1][j] || dp[i - 1][j - curr]
+    }
+  }
+  return !!dp[len][sum]
+}
+
+// 动态规划 (数组降维)
+const canPartition = nums => {
+  let len = nums.length, sum = 0
+  for (let i = 0; i < len; i++) {
+    sum += nums[i]
+  }
+  if (sum % 2 !== 0) return false
+  sum = sum / 2
+  let dp = [1]
+  for (let i = 1; i <= len; i++) { // 物品序号
+    let curr = nums[i - 1]
+    for (let j = sum; j >= curr; j--) { // 背包容量
+      dp[j] = dp[j] || dp[j - curr]
+    }
+    if (dp[sum]) return true
+  }
+  return !!dp[sum]
+}
