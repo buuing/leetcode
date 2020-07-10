e0cf63968a1698d8487519a72d4bb579c6a33991
buuing <17778888138@189.cn>
2020-07-10 10:03:06 +0800

    修复bug

--- medium/79.word-search.js
+++ medium/79.word-search.js
@@ -1,18 +1,25 @@
 // 回溯算法+剪枝
 const exist = (board, word) => {
   if (!board.length || !board[0].length) return false
-  const dfs = (start, i, j) => {
-    if (i < 0 || i > board.length) return false
-    if (j < 0 || j > board[0].length) return false
+  let map = new Array(board.length).fill().map(_ => [])
+  const dfs = (start, i, j, map) => {
+    if (i < 0 || i >= board.length) return false
+    if (j < 0 || j >= board[0].length) return false
+    if (map[i][j]) return false
     if (board[i][j] !== word[start]) return false
-    if (dfs(start + 1, i - 1, j)) return true
-    if (dfs(start + 1, i + 1, j)) return true
-    if (dfs(start + 1, i, j - 1)) return true
-    if (dfs(start + 1, i, j + 1)) return true
+    if (start === word.length - 1) return true
+    map[i][j] = 1
+    if (
+      dfs(start + 1, i - 1, j, map) ||
+      dfs(start + 1, i + 1, j, map) ||
+      dfs(start + 1, i, j - 1, map) ||
+      dfs(start + 1, i, j + 1, map)
+    ) return true
+    map[i][j] = 0
   }
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
-      if (board[i][j] === word[0] && dfs(1, i, j)) return true
+      if (board[i][j] === word[0] && dfs(0, i, j, JSON.parse(JSON.stringify(map)))) return true
     }
   }
   return false
