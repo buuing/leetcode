ed572ecf69b34f8795a77a52475bc8309b243bf1
buuing <17778888138@189.cn>
2020-07-30 21:55:43 +0800

    增加记忆化递归解题

--- medium/279.perfect-squares.js
+++ medium/279.perfect-squares.js
@@ -1,6 +1,19 @@
+// 记忆化递归 (性能极低)
+const memo = [0, 1]
+const numSquares = n => {
+  if (Math.sqrt(n) % 1 === 0) memo[n] = 1
+  if (!memo[n]) {
+    let min = n
+    for (let i = 1; i <= n / 2; i++) {
+      min = Math.min(numSquares(i) + numSquares(n - i), min)
+    }
+    memo[n] = min
+  }
+  return memo[n]
+}
+
 // 动态规划 (性能较低)
 const numSquares = n => {
-  if (Math.sqrt(n) % 1 === 0) return 1
   const dp = [0, 1]
   for (let i = 2; i <= n; i++) {
     if (Math.sqrt(i) % 1 === 0) {
@@ -9,8 +22,7 @@ const numSquares = n => {
     }
     let min = i
     for (let j = 1; j <= i / 2; j++) {
-      let curr = dp[j] + dp[i - j]
-      if (min > curr) min = curr
+      min = Math.min(min, dp[j] + dp[i - j])
     }
     dp[i] = min
   }
@@ -23,8 +35,7 @@ const numSquares = n => {
   for (let i = 2; i <= n; i++) {
     let min = i
     for (let j = 1; j * j <= i; j++) {
-      let curr = dp[i - j * j] + 1
-      if (min > curr) min = curr
+      min = Math.min(min, dp[i - j * j] + 1)
     }
     dp[i] = min
   }
