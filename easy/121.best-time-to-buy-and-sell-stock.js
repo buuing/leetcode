bb029072d1cc803918c46576aa2c6fb56bb49d4b
buuing <17778888138@189.cn>
2020-07-30 21:55:26 +0800

    增加dp解题

--- easy/121.best-time-to-buy-and-sell-stock.js
+++ easy/121.best-time-to-buy-and-sell-stock.js
@@ -1,7 +1,4 @@
-
-/**
- * 每次都比较前一次和当前到最小的差值
- */
+// 数组解题, 每次都比较前一次和当前到最小的差值
 const maxProfit = prices => {
   if (prices.length < 2) return 0
   let min = prices[0], res = 0
@@ -12,4 +9,19 @@ const maxProfit = prices => {
     }
   }
   return res
+}
+
+// 动态规划 (效率较低)
+const maxProfit = prices => {
+  let len = prices.length
+  if (len < 2) return 0
+  let dp = new Array(len).fill(0)
+  for (let i = len - 2; i >= 0; i--) {
+    let max = 0
+    for (let j = i + 1; j < len; j++) {
+      max = Math.max(prices[j] - prices[i], max)
+    }
+    dp[i] = Math.max(dp[i + 1], max)
+  }
+  return dp[0]
 }
