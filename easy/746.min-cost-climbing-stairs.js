677185687b1996714a7531c7f3b5a98a9a689c5e
buuing <17778888138@189.cn>
2020-12-21 22:23:56 +0800

    爬楼梯升级版-dp解

--- easy/746.min-cost-climbing-stairs.js
+++ easy/746.min-cost-climbing-stairs.js
@@ -15,3 +15,16 @@ const minCostClimbingStairs = cost => {
   dfs(cost.length - 1)
   return memo[cost.length - 1]
 }
+
+// 动态规划
+const minCostClimbingStairs = cost => {
+  cost.push(0)
+  const dp = []
+  for (let i = 0; i < cost.length; i++) {
+    dp[i] = Math.min(
+      ~~dp[i - 1] + ~~cost[i - 1],
+      ~~dp[i - 2] + ~~cost[i - 2]
+    )
+  }
+  return dp[cost.length - 1]
+}
